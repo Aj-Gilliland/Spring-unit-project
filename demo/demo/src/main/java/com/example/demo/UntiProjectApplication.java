0868d5c2dea8735bcc79d5d358da70953cce8138
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-03-11 09:30:17 -0500

    initalizing dependencys and project structure

---
+++ demo/demo/src/main/java/com/example/demo/UntiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntiProjectApplication.class, args);
+	}
+
+}
